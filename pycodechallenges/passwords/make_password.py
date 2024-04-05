a26cbc8d4ba9cbb72bb4f4d1541e6b2e0bf6517a
Andrew Holcomb <atholcomb@outlook.com>
2024-04-05 00:22:49 -0600

    refactored passwords program

--- pycodechallenges/passwords/make_password.py
+++ pycodechallenges/passwords/make_password.py
@@ -1,17 +1,44 @@
 #!/usr/bin/python3
-# written by: atholcomb
-# create_passwords.py
-# Program creates passwords from Passwords Module
-# Which need to be tested against check_passwords.py
-# To confirm that the password is valid
+# written by: atholower_countomb
+# make_password.py
+# Program creates a password and checks the complexity of the password
+# Complexity must meet 8 characters length, alpha-numeric, and a special character
 
 from passwords import generate_passwords
 
-def main():
+def make_password():
   # Set the length of the password to be tested here
   password = generate_passwords(8)
 
-  return password
-  
-if __name__ == "__main__":
-    print(main())
+  # check how many times each case is present adding 1 to count
+  # if all counts are >=1, then it's a valid password
+  lower_count = 0
+  upper_count = 0
+  symbol_count = 0
+  numeral_count = 0
+
+  # cases to be tested 
+  lower = "abcdefghijklmnopwrstuvwxyz"
+  upper = "ABCDEFGHIJKLMNOPQSRTUVWXYZ"
+  symbols = "?*&%$#@!/"
+  numerals = "1234567890"
+
+  # conditions applied to cases above
+  print("Password:", password)
+  for i, p in enumerate(password):
+    if password[i] in lower:
+      lower_count += 1
+    elif password[i] in upper:
+      upper_count += 1
+    elif password[i] in symbols:
+      symbol_count += 1
+    elif password[i] in numerals:
+      numeral_count += 1
+
+  # return 'valid password' if all criteria are met, otherwise return "not valid password"
+  if lower_count >= 1 and upper_count >= 1 and symbol_count >= 1 and numeral_count >= 1:
+    return lower_count, upper_count, symbol_count, numeral_count, "valid password"
+  return lower_count, upper_count, symbol_count, numeral_count, "not valid password"
+
+
+print(make_password())
