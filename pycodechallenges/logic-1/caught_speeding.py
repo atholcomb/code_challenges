296ea3be6ed590f822d67c48a4091ca74c3eed92
Andrew Holcomb <atholcomb@outlook.com>
2024-04-11 17:23:08 -0600

    updating repo

--- pycodechallenges/logic-1/caught_speeding.py
+++ pycodechallenges/logic-1/caught_speeding.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 # written by: atholcomb
 # caught_speeding.py
+# Program should return True if speed is above 60
+# and False otherwise with also an exception of a birthday
 
 def caught_speeding(speed, is_birthday):
-  if is_birthday == True:
-    return 0
-  elif speed <= 60:
-    return 0 
-  elif speed > 60 and speed <= 80:
-    return 1
+  if speed <= 60 and is_birthday == True:
+    return True
+  elif speed <= 60 and is_birthday == False:
+    return True
+  elif speed > 60 and speed <= 80 and is_birthday == True:
+    return True
   else:
-    return 2
+    return False
 
 
-print(caught_speeding(60, False))
+print(caught_speeding(60, True))
 print(caught_speeding(65, False))
 print(caught_speeding(65, True))
 print(caught_speeding(85, True))
