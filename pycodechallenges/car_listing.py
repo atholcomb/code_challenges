79868d5432ca7ed45242d31ebd18b46d491b0a24
Andrew Holcomb <atholcomb@outlook.com>
2024-11-20 07:18:29 -0700

    car_listing.py

--- pycodechallenges/car_listing.py
+++ pycodechallenges/car_listing.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # written by: atholcomb
 # car_listing.py
+# Program outputs a cars cost in dollar amounts
 
 def car_listing(car_prices):
-  result = ""
-  for k, v in car_prices.items():
-    result += "{} costs {} dollars".format(k, v) + "\n"
+  result = {}
+  for model, price in car_prices.items():
+    result[model] = price
   return result
 
 print(car_listing({"Kia Soul":19000, "Lamborghini Diablo":55000, "Ford Fiesta":13000, "Toyota Prius":24000}))
