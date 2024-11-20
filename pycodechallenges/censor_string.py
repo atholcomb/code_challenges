f6b9c98cd7fe35dba08430d0a4b3147666b8084c
Andrew Holcomb <atholcomb@outlook.com>
2024-11-20 07:25:39 -0700

    refactored censor_string.py

--- pycodechallenges/censor_string.py
+++ pycodechallenges/censor_string.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 # written by: atholcomb
 # censor_string.py
+# For a given text string, censor the word inside the list with the given char
 
-def censor_string(txt, lst, character):
-    for word in lst:
-        txt = txt.replace(word, character*len(word))
-    return txt
+def censor_string(text_str, censor_list, censor_character):
+  for word in censor_list:
+    text_str = text_str.replace(word, censor_character*len(word))
+  
+  return text_str
 
 print(censor_string("Today is a Wednesday!", ["Today", "a"], "-"))
 print(censor_string("The cow jumped over the moon.", ["cow", "moon"], "*"))
