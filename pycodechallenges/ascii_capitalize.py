ad96c82277df5ae8465afa97db7fa7db5712c81f
Andrew Holcomb <atholcomb@outlook.com>
2024-11-20 06:24:21 -0700

    refactored ascii_capitalize.py

--- pycodechallenges/ascii_capitalize.py
+++ pycodechallenges/ascii_capitalize.py
@@ -5,21 +5,20 @@
 # code is even and returns its lower case version if its ASCII code is odd.
 
 def ascii_capitalize(string):
-    string = string.lower()
-    ascii_values = []
-    new_string = []
-    
-    for char in string:
-        ascii_values.append(ord(char))
+  string = string.lower()
+  ascii_values = []
+  new_string = []
+  
+  for char in string:
+      ascii_values.append(ord(char))
 
-    for av in ascii_values:
-        if av % 2 == 0:
-            new_string.append(chr(av).upper())
-        else:
-            new_string.append(chr(av))
+  for av in ascii_values:
+      if av % 2 == 0:
+          new_string.append(chr(av).upper())
+      else:
+          new_string.append(chr(av))
 
-    return ''.join(new_string)
-
+  return ''.join(new_string)
 
 print(ascii_capitalize("to be or not to be!"))              # "To Be oR NoT To Be!"
 print(ascii_capitalize("THE LITTLE MERMAID"))               # "THe LiTTLe meRmaiD"
