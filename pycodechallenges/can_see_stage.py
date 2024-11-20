94aa13f6f4bd061a6dee02fae388bb75c53e9456
Andrew Holcomb <atholcomb@outlook.com>
2024-11-20 07:04:00 -0700

    refactored can_see_stage.py

--- pycodechallenges/can_see_stage.py
+++ pycodechallenges/can_see_stage.py
@@ -5,17 +5,14 @@
 # A number can "see" the front-stage if it is strictly greater than the number 
 # before it.
 
-
 def can_see_stage(seats):
-
   for idx, row in enumerate(seats):
-    # determine if elements in columns are greater than the # before it
+  # determine if elements in columns are greater than the # before it
     if seats[idx-1][0] > seats[idx-2][0] > seats[idx-3][0]:
       if seats[idx-1][1] > seats[idx-2][1] > seats[idx-3][1]:
         if seats[idx-1][2] > seats[idx-2][2] > seats[idx-3][2]:
           return True
     return False
-
 
 # True
 print(can_see_stage([
