71ce662a3ccc311bb1d6c4c278a0216d1722aeaa
Andrew Holcomb <atholcomb@outlook.com>
2024-11-20 13:23:00 -0700

    common_elements.py

--- pycodechallenges/common_elements.py
+++ pycodechallenges/common_elements.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 # written by: atholcomb
 # common_elements.py
+# For two given lists, create a new set of values
+# from both making sure to only find unique values
 
-def commonElements(listA, listB):
-    result = set() # create a set with only unique values
-    
-    for item in listA:
-        if item in listB:
-            result.add(item) # add the correct value to the set
-    
-    return list(result) # convert the set into a list for ouput
+def common_elements(list_a, list_b):
+  # create a set with only unique values
+  result = set()
 
+  # add the correct value to the set
+  for item in list_a:
+    if item in list_b:
+      result.add(item) 
 
+  # convert the set into a list for ouput
+  return f"Original List: {list_a} -> Unique Value(s): {result}" 
 
-
-print(commonElements([-1, 3, 4, 6, 7, 9], [1, 3]))
-print(commonElements([1, 3, 4, 6, 7, 9], [1, 2, 3, 4, 7, 10]))
-print(commonElements([1, 2, 2, 2, 3, 4, 5], [1, 2, 4, 5]))
-print(commonElements([1, 2, 3, 4, 5], [10, 12, 13, 15]))
+print(common_elements([-1, 3, 4, 6, 7, 9], [1, 3]))
+print(common_elements([1, 3, 4, 6, 7, 9], [1, 2, 3, 4, 7, 10]))
+print(common_elements([1, 2, 2, 2, 3, 4, 5], [1, 2, 4, 5]))
+print(common_elements([1, 2, 3, 4, 5], [10, 12, 13, 15]))
