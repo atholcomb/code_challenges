a7ae0a789fd8cbf70051637ed1ab868f67700adc
Andrew Holcomb <atholcomb@outlook.com>
2024-11-20 06:16:39 -0700

    refactored anagram.py

--- pycodechallenges/anagram.py
+++ pycodechallenges/anagram.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 # written by: atholcomb
 # anagram.py
+# Program identifies if the two strings passed in are anagrams
 
-def is_anagram(str1, str2):
-
-  if sorted(str1) == sorted(str2):
+def is_anagram(str_one, str_two):
+  if sorted(str_one) == sorted(str_two):
     return True
   return False
-
 
 print(is_anagram("typhoon", "opython"))
 print(is_anagram("Alice", "Bob"))
