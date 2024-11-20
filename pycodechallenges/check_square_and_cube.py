7a8c36d11ba73d0111eae4de7784117432ce24d3
Andrew Holcomb <atholcomb@outlook.com>
2024-11-20 07:56:07 -0700

    refactored check_square_and_cube.py

--- pycodechallenges/check_square_and_cube.py
+++ pycodechallenges/check_square_and_cube.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 # written by: atholcomb
 # check_square_and_cube.py
-# Create a function that takes a list of two numbers and checks if the square root 
-# of the first number is equal to the cube root of the second number.
+# Create a function that takes a list of two numbers and checks if the square 
+# root of the first number is equal to the cube root of the second number.
 
 import math
 
-def check_square_and_cube(s_c_list):
-    for number in s_c_list:
-        square = int(math.sqrt(number))
-        cube = math.ceil(number ** (1/3))
-        if square == cube:
-            return f'square: {square}, cube: {cube} -> True'
-        return f'square: {square}, cube: {cube} -> False'
+def check_square_and_cube(squared_and_cubed_list):
+  square_root = math.sqrt(squared_and_cubed_list[0])
+  cube_root = round(math.cbrt(squared_and_cubed_list[1]), 1)
 
+  if square_root != cube_root:
+    return f"{square_root}, {cube_root} -> False"
+  return f"{square_root}, {cube_root} -> True"
 
-print(check_square_and_cube([4, 8])) # True
-print(check_square_and_cube([16, 48])) # False
-print(check_square_and_cube([9, 27])) # True
+print(check_square_and_cube([4, 8]))
+print(check_square_and_cube([16, 48])) 
+print(check_square_and_cube([9, 27]))
