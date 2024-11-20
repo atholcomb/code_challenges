7adc69713f948eeca78017d8df6c6036b11f223b
Andrew Holcomb <atholcomb@outlook.com>
2024-11-20 12:30:26 -0700

    refactored combine_lists.py

--- pycodechallenges/combine_lists.py
+++ pycodechallenges/combine_lists.py
@@ -5,7 +5,7 @@
 # followed by the elements of list1 in reverse order
 
 def combine_lists(list1, list2):
-  print(f"Original Order: {list1 + list2}")
+  print("Original Order:", list1 + list2)
   return f"Reversed Order: {list1[::-1] + list2}"
 	
 print(combine_lists(["Alice", "Cindy", "Bobby", "Jan", "Peter"], ["Mike", "Carol", "Greg", "Marcia"]))
