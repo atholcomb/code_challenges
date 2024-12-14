d03354d4f49341b33948f71bebc13e4f9479a841
Andrew Holcomb <atholcomb@outlook.com>
2024-12-14 02:18:51 -0700

    refactored decimal_part.py

--- pycodechallenges/decimal_part.py
+++ pycodechallenges/decimal_part.py
@@ -3,19 +3,19 @@
 # decimal_part.py
 # Create a function which takes a number n and return its decimal part.
 
-def decimal_part(n):
-    # create str object out of float(n) to enumerate over
-    n_string = str(n)
-    
-    # find the position where the decimal is at
-    # return str with decimal part only
-    # else return n
-    for pos, nstr in enumerate(n_string):
-        if '.' in n_string[pos]:
-            return f'0{n_string[pos:]}'
-    return 0
+def decimal_part(float_num):
+  # create str object out of float_num to enumerate over
+  float_str = str(float_num)
+  
+  # find the position where the decimal is at
+  # return str with decimal part only
+  # else return n
+  for idx, fstr in enumerate(float_str):
+    if '.' in float_str[idx]:
+      return f'{float_str[idx:]}'
+  return 0
         
 
-print(decimal_part(1.2))    # 0.2
-print(decimal_part(-3.73))  # 0.73
+print(decimal_part(1.2))    # .2
+print(decimal_part(-3.73))  # .73
 print(decimal_part(10))     # 0
