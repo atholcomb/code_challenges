0fef16c32780daacc1fc38d75a60e4a14131a3d2
Andrew Holcomb <atholcomb@outlook.com>
2024-12-31 14:36:13 -0700

    updated anagram.py

--- pycodechallenges/anagram.py
+++ pycodechallenges/anagram.py
@@ -3,10 +3,24 @@
 # anagram.py
 # Program identifies if the two strings passed in are anagrams
 
-def is_anagram(str_one, str_two):
-  if sorted(str_one) == sorted(str_two):
-    return True
-  return False
+print("String#1\tString#2\tanagram")
 
-print(is_anagram("typhoon", "opython"))
+def is_anagram(str_one, str_two):
+  str_one = str_one.lower()
+  str_two = str_two.lower()
+  
+  if ''.join(sorted(str_one)) in ''.join(sorted(str_two)):
+    return f"{str_one}\t\t{str_two}\t\tTrue"
+  else: 
+    return f"{str_one}\t\t{str_two}\t\tFalse"
+
+
 print(is_anagram("Alice", "Bob"))
+print(is_anagram("Peter", "Andrew"))
+print(is_anagram("Teddy", "Robot"))
+print(is_anagram("Angel", "glean"))
+print(is_anagram("arc", "car"))
+print(is_anagram("bored", "robed"))
+print(is_anagram("Listen", "Silent"))
+print(is_anagram("hello", "oellh"))
+print(is_anagram("typhoon", "opython"))
