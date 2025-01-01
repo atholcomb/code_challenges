80daa73fe0b4cc3bc15368bf668a631f28e586f3
Andrew Holcomb <atholcomb@outlook.com>
2025-01-01 12:52:48 -0700

    refactored

--- pycodechallenges/consec_numbers.py
+++ pycodechallenges/consec_numbers.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 # written by: atholcomb
 # consec_numbers.py
-# Create a function that determines whether elements in an array can be re-arranged
-# to form a consecutive list of numbers where each number appears exactly once.
+# Create a function that determines whether elements in an array can be 
+# re-arranged to form a consecutive list of numbers where each number 
+# appears exactly once.
 
-def consec_numbers(lst):
-    
-    for num in lst:
-        if lst[::-1] > lst[0:]:
-            return False
-        else:
-            return sorted(lst) 
+def consec_numbers(elements):
+  numbers = [1,2,3,4,5,6,7,8,9,10]
+  
+  for number in numbers:
+    if number in sorted(elements):
+      return True
+  return False
+
+  #for num in elements:
+  #  if elements[::-1] > elements[0:]:
+  #      return False
+  #  else:
+  #      return sorted(elements) 
 
 print(consec_numbers([5, 1, 4, 3, 2])) # [1, 2, 3, 4, 5]
 print(consec_numbers([5, 1, 4, 3, 2, 8])) # False
