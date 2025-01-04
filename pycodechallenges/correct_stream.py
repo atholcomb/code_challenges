b04280744cb9488fd7cc6a2c8e76fd6720a27b0b
Andrew Holcomb <atholcomb@outlook.com>
2025-01-04 08:18:56 -0700

    refactored correct_stream.py

--- pycodechallenges/correct_stream.py
+++ pycodechallenges/correct_stream.py
@@ -5,17 +5,16 @@
 # and the list of correctly-typed words and outputs a list 
 # containing 1s (correctly-typed words) and -1s (incorrectly-typed words).
 
-def correct_stream(correctlist, userlist):
-    count = []
-    
-    for cl in correctlist:
-        if cl in userlist:
-            count.append(1)
-        else:
-            count.append(-1)
+def correct_stream(userlist, correctlist):
+  output = []
+  
+  for ul in userlist:
+    if ul in correctlist:
+      output.append(1)
+    else:
+      output.append(-1)
 
-    return count
-
+  return output
 
 print(correct_stream(["it", "is", "find"], ["it", "is", "fine"])) # [1, 1, -1]
 print(correct_stream(["april", "showrs", "bring", "may", "flowers"], ["april", "showers", "bring", "may", "flowers"])) # [1, -1, 1, 1, 1]
