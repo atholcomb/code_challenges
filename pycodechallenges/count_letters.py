b03d6135a3c0479d559192a290767dcf07922828
Andrew Holcomb <atholcomb@outlook.com>
2025-01-04 08:24:55 -0700

    refactored count_letters.py

--- pycodechallenges/count_letters.py
+++ pycodechallenges/count_letters.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
 # written by: atholcomb
 # count_letters.py
+# count the number of characters for each string given
 
-def count_letters(text):
-    result = {}
-    text = text.lower()
-    text = text.replace(' ', '')
-    
-    if '!' in text:
-        t1 = text.index('!')
-        text = text[:t1]
-    elif '.' in text:
-        t2 = text.index('.')
-        text = text[:t2]
-    
-    for letter in text:
-        if letter in text:
-            result[letter] = text.count(letter)
-    
-    return result
+def count_letters(string):
+  result = {}
 
+  for s in string:
+    result[s] = string.count(s)
 
-print(count_letters("AaBbCc"))
-# Should be {'a': 2, 'b': 2, 'c': 2}
+  return result
 
-print(count_letters("Math is fun! 2+2=4"))
-# Should be {'m': 1, 'a': 1, 't': 1, 'h': 1, 'i': 1, 's': 1, 'f': 1, 'u': 1, 'n': 1}
-
-print(count_letters("This is a sentence."))
-# Should be {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}
+print(count_letters("AaBbCc"))              # Should be {'a': 2, 'b': 2, 'c': 2}
+print(count_letters("Math is fun! 2+2=4"))  # Should be {'m': 1, 'a': 1, 't': 1, 'h': 1, 'i': 1, 's': 1, 'f': 1, 'u': 1, 'n': 1}
+print(count_letters("This is a sentence.")) # Should be {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}
