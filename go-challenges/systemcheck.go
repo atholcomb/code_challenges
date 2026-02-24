e5311d659ddf46d392e5e74b2a44c761a6523ffe
Andrew Holcomb <atholcomb@outlook.com>
2026-02-24 12:43:24 -0700

    renamed systemCheck.go -> systemcheck.go

---
+++ go-challenges/systemcheck.go
@@ -0,0 +1,36 @@
+/*
+/* Written By: atholcomb
+/* main.go
+/* Program outputs a mock system check analysis
+*/
+
+package main
+
+import ( 
+  "fmt"
+  "time"
+)
+
+func main() {
+  env := sysCheck()
+
+  fmt.Printf("%s%s\n", "Connected to system: ", "glk-ath-2427-OMA")
+  fmt.Println("***Reading attributes***")
+
+  for k, v := range env {
+    time.Sleep(1 * time.Second)
+    fmt.Printf("%v: %v\n", k, v)
+  }
+}
+
+func sysCheck() map[string]string {
+  env := map[string]string {
+    "os": "linux",
+    "cpu_cores": "24",
+    "gpu_type": "cuda xtreme",
+    "ram_size": "2Tb",
+    "kernel": "3.2.1",
+    "vm": "yes",
+  }
+  return env
+}
